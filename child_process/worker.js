43ca3163e6dae5af60ef6b789eedf01809123f46
Charles Zhao <anseszcw@gmail.com>
2018-05-21 01:29:35 +0800

    add child_process

---
+++ child_process/worker.js
@@ -0,0 +1,21 @@
+var http = require('http')
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/plain' })
+    res.end('handled by child, pid is ' + process.pid + '\n')
+})
+
+var worker
+process.on('message', function (m, tcp) {
+    if (m === 'server') {
+        worker = tcp
+        worker.on('connection', function (socket) {
+            server.emit('connection', socket)
+        })
+    }
+})
+
+process.on('uncaughtException', function () {
+    worker.close(function () {
+        process.exit(1)
+    })
+})
